08fbea571c8c220b3f77ced7e0804427162f9761
KonradRudnicki <97458824+KonradRudnicki@users.noreply.github.com>
2022-01-30 12:03:57 +0100

    First commit

---
+++ TicTacToe/src/main/java/com/KonradRudnicki/TicTacToe/TicTacToeApplication.java
@@ -0,0 +1,21 @@
+package com.KonradRudnicki.TicTacToe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeApplication.class, args);
+	}
+
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+}
---
+++ TicTacToe/src/test/java/com/KonradRudnicki/TicTacToe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KonradRudnicki.TicTacToe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
